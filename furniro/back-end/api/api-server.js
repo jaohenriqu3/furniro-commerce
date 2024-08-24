55e0b3d5c7331c876f0dff86db0089bcca4c792e
jaohenriqu3 <jhlucena2004@gmail.com>
2024-08-24 02:35:32 -0300

    FIX: resolvendo bugs do server, conectando com banco de dados

--- furniro/back-end/api/api-server.js
+++ furniro/back-end/api/api-server.js
@@ -1,26 +1,34 @@
-const http = require('http');
-const { PrismaClient } = require('@prisma/client');
+import http from 'http';
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 const server = http.createServer(async (req, res) => {
-  if (req.method === 'GET' && req.url.startsWith('/api/products')) {
-    const id = req.url.split('/').pop(); // Pega o ID da URL
+  const urlParts = req.url?.split('/') || [];
+  const isProductsRoute = urlParts[1] === 'api' && urlParts[2] === 'products';
 
+  if (req.method === 'GET' && isProductsRoute && urlParts.length === 3) {
     try {
-      if (id) {
-        const product = await prisma.product.findUnique({ where: { id: parseInt(id, 10) } });
-        if (product) {
-          res.writeHead(200, { 'Content-Type': 'application/json' });
-          res.end(JSON.stringify(product));
-        } else {
-          res.writeHead(404, { 'Content-Type': 'text/plain' });
-          res.end('Product not found');
-        }
-      } else {
-        const products = await prisma.product.findMany();
+      const products = await prisma.product.findMany();
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(products));
+    } catch (error) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Internal Server Error');
+    }
+  } else if (req.method === 'GET' && isProductsRoute && urlParts.length === 4) {
+    const productId = urlParts[3];
+    try {
+      const product = await prisma.product.findUnique({
+        where: { id: productId },
+      });
+
+      if (product) {
         res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(products));
+        res.end(JSON.stringify(product));
+      } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Product Not Found');
       }
     } catch (error) {
       res.writeHead(500, { 'Content-Type': 'text/plain' });
@@ -35,3 +43,5 @@ const server = http.createServer(async (req, res) => {
 server.listen(3001, () => {
   console.log('API server running on http://localhost:3001');
 });
+
+
